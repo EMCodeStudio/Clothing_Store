db004665e6e32af7b0d7c7ba9d28ce58678dca1a
EMCodeStudio <estivenmc53@gmail.com>
2023-12-05 18:06:30 -0500

    creating profile option copmponent

---
+++ Apps/Client/src/Components/Menu/UserData.ts
@@ -0,0 +1,5 @@
+export const user = {
+    name: 'Jane Spoonfighter',
+    email: 'janspoon@fighter.dev',
+    image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png',
+};
