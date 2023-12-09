53110f686db90e18c69c6ff7abd70bdb78705097
EMCodeStudio <estivenmc53@gmail.com>
2023-12-08 20:23:22 -0500

    fixing search hidden on outside click

--- Apps/Client/src/Components/Menus/MunuParts/SearchField.tsx
+++ Apps/Client/src/Components/Menus/MunuParts/SearchField.tsx
@@ -1,4 +1,3 @@
-
 import {
     ActionIcon,
     TextInput,
@@ -6,13 +5,9 @@ import {
     rem,
     useMantineTheme,
 } from '@mantine/core';
-import { Autocomplete } from '@mantine/core';
 import { IconArrowRight, IconSearch } from '@tabler/icons-react';
-import classes from '../HeaderMegaMenu.module.css';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { dataProduct } from '../MenuData/ProductData';
-
-
 interface Product {
     id: number;
     title: string;
@@ -24,6 +19,8 @@ function SearchField(props: TextInputProps) {
     const theme = useMantineTheme()
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState<Product[]>([]);
+    const [isSearchVisible, setIsSearchVisible] = useState(true);
+    const searchRef = useRef<HTMLInputElement | null>(null);
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         const inputTerm = e.target.value
@@ -38,6 +35,7 @@ function SearchField(props: TextInputProps) {
         );
 
         setSearchResults(results);
+        setIsSearchVisible(true); 
     };
 
     const handleProductClick = (productId: number) => {
@@ -48,50 +46,73 @@ function SearchField(props: TextInputProps) {
         console.log('Letras ingresadas:', searchTerm);
     };
 
-    return (
-        <>
-            <div className="search-container">
-                <TextInput
-                    value={searchTerm}
-                    onChange={handleSearch}
-                    radius="xl"
-                    size="md"
-                    placeholder="Realizar Busqueda..."
-                    rightSectionWidth={42}
-                    leftSection={
-                        <IconSearch style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-                    }
+    const handleClickOutsideSearch = (e: MouseEvent) => {
+        if (searchRef.current && !searchRef.current.contains(e.target as Node)) {
+            setIsSearchVisible(false);
+        }
+    };
+    
 
-                    rightSection={
-                        <ActionIcon
-                            onClick={handleSearchButton}
-                            size={32}
-                            radius="xl"
-                            color={theme.primaryColor}
-                            variant="filled">
-                            <IconArrowRight style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-                        </ActionIcon>
-                    }
-                    {...props}
-                />
-                {
-                    searchResults.length > 0 &&
-                    (
-                        <div className="results-container">
-                            {searchResults.map(product => (
-                                <div
-                                    key={product.id}
-                                    className="result-item"
-                                    onClick={() => handleProductClick(product.id)}
-                                >
-                                    {product.title}
-                                </div>
-                            ))}
-                        </div>
-                    )
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutsideSearch);
+        return () => {
+            document.removeEventListener('click', handleClickOutsideSearch);
+        };
+    }, []);
+
+    return (
+
+        <div className="search-container" ref={searchRef}>
+
+
+            <TextInput
+                value={searchTerm}
+                onChange={handleSearch}
+                radius="xl"
+                size="md"
+                placeholder="Realizar Busqueda..."
+                rightSectionWidth={42}
+                leftSection={
+                    <IconSearch style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
                 }
-            </div>
-        </>
+
+                rightSection={
+                    <ActionIcon
+                        onClick={handleSearchButton}
+                        size={32}
+                        radius="xl"
+                        color={theme.primaryColor}
+                        variant="filled">
+                        <IconArrowRight style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+                    </ActionIcon>
+                }
+
+                {...props}
+            />
+            {isSearchVisible && (
+                <>
+                    {
+                        searchResults.length > 0 &&
+                        (
+                            <div className="results-container">
+                                {searchResults.map(product => (
+                                    <div
+                                        key={product.id}
+                                        className="result-item"
+                                        onClick={() => handleProductClick(product.id)}
+                                    >
+                                        {product.title}
+                                    </div>
+                                ))}
+                            </div>
+
+                        )
+
+                    }
+                </>
+            )}
+        </div>
+
     )
 }
 
